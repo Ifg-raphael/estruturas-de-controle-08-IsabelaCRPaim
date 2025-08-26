91d5f689742a52b493a445c9447d91868c4a91c7
Isabela <paim.isabela@hotmail.com>
2025-08-26 16:03:58 -0300

    implementação finalizada

--- calcula_peso_ideal.py
+++ calcula_peso_ideal.py
@@ -1,1 +1,9 @@
 # Sua solução aqui
+altura = float(input("Digite a altura: "))
+sexo = input("Digite o sexo (M para masculino, F para feminino): ")
+
+if sexo == "M":
+    peso_ideal = (72.7*altura) - 58
+else:
+    peso_ideal = (62.1*altura)- 44.7
+print ( f"{peso_ideal:.2f}")
